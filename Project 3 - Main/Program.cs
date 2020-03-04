29c75ff339eae1f6611112c660dd2ad7e0e6ca2a
Laurent Wolf <laurent.wolf1983@gmail.coml>
2020-03-04 16:24:07 +0100

    Initial load

---
+++ Project 3 - Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_3___Press_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
