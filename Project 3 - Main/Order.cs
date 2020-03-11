ed7c5c73367ff5443d5b8674deefc09355eea326
Kevin Sartori <kevin.bouty@hotmail.fr>
2020-03-11 15:56:49 +0100

    Removed the link between order-editor

--- Project 3 - Main/Order.cs
+++ Project 3 - Main/Order.cs
@@ -9,7 +9,6 @@ namespace Project_3___Press_Project
         public Guid OrderId { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime DeliveryDate { get; set; }
-        public Editor Editor { get; set; }
         public Shop Shop { get; set; }
         public User User { get; set; }
 
