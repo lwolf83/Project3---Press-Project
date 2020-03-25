57680ea34100e419be5eebad817fed9048d8c750
Kevin Sartori <kevin.bouty@hotmail.fr>
2020-03-25 19:59:02 +0100

    [+] Display shops * Display every shops in UserShop * Started shop filtering implementation in userShop

--- Project 3 - Main/DatabasePopulator.cs
+++ Project 3 - Main/DatabasePopulator.cs
@@ -33,7 +33,7 @@ namespace Project_3___Press_Project
             populator.ImportCitiesInDB();
             populator.LinkLocations();
 
-            populator.CreateAddresses(5);
+            populator.CreateAddresses(100);
             populator.CreateShops();
             populator.CreateEditors(3);
             populator.CreateNewspapers(3);
@@ -138,12 +138,15 @@ namespace Project_3___Press_Project
             int adressesCounter = 0;
             foreach (City city in randomCities)
             {
-                Address address = new Address();
-                address.StreetNumber = $"{randomGenerator.Next(1,1000)}";
-                address.StreetName = $"street n° {adressesCounter};{randomGenerator.Next(1,100)}";
-                address.City = city;
-                adressesCounter++;
-                context.Add(address);
+                for (int i = 0; i < 10; i++)
+                {
+                    Address address = new Address();
+                    address.StreetNumber = $"{randomGenerator.Next(1, 1000)}";
+                    address.StreetName = $"street n° {adressesCounter};{randomGenerator.Next(1, 100)}";
+                    address.City = city;
+                    adressesCounter++;
+                    context.Add(address);
+                }
             }
             context.SaveChanges();
             
