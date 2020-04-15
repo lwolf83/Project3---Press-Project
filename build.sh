bd9d874fa05b1663701d3dbede55752ca580b6d0
PREVOST Corentin <corentin.prevost@wildcodeschool.fr>
2020-04-15 13:48:49 +0200

    [*] Delete useless scripts

--- build.sh
+++
@@ -1,2 +0,0 @@
-dotnet restore 'Project 3 - Main/Project 3 - Press Project.csproj'
-dotnet build 'Project 3 - Main/Project 3 - Press Project.csproj'
--- test.sh
+++
@@ -1,5 +0,0 @@
-cd TestPressProject 
-dotnet add package NUnit.Console
-dotnet restore TestPressProject.csproj
-dotnet build TestPressProject.csproj
-dotnet test TestPressProject.csproj
