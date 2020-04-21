e616449d0f96e860fa6dfd0529cb62d30a3342e9
Kevin Sartori <kevin.bouty@hotmail.fr>
2020-04-21 19:09:25 +0200

    Resolved #109 and #97

---
+++ Project 3 - Main/Factory/NewspaperFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_3___Press_Project
+{
+    public class NewspaperFactory
+    {
+        public static Newspaper Create(string name, int periodiciy, string ean13, decimal price, List<Catalog> catalogs, Editor editor)
+        {
+            Newspaper newspaper = new Newspaper();
+            newspaper.Name = name;
+            newspaper.Periodicity = periodiciy;
+            newspaper.EAN13 = ean13;
+            newspaper.Price = price;
+            newspaper.Catalogs = catalogs;
+            newspaper.Editor = editor;
+
+            return newspaper;
+        }
+    }
+}
---
+++ Project 3 - Main/Loading/EditorLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Project_3___Press_Project
+{
+    public static class EditorLoader
+    {
+        public static Editor Get(Editor editor)
+        {
+            using (var context = new PressContext())
+            {
+                var editorInDb = (from i in context.Editors
+                              where i.EditorId == editor.EditorId
+                              select i).First();
+            }
+            return editor;
+        }
+    }
+}
